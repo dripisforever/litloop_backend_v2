61375c7b6bcba86332d05acb5fe5739fd0eab96d
Flipworld <talga.tawit@gmail.com>
2025-06-10 05:09:26 +0500

    updated some files 5AM

--- users/models.py
+++ users/models.py
@@ -4,8 +4,7 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 from rest_framework_simplejwt.tokens import RefreshToken
-from imagekit.models import ProcessedImageField
-from imagekit.processors import ResizeToFill
+
 
 from users.managers import UserManager
 
@@ -18,7 +17,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_verified = models.BooleanField(default=False)
     is_active   = models.BooleanField(default=True)
     is_staff    = models.BooleanField(default=False)
-    
+
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
 
