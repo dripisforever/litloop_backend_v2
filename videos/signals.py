e4f56b02d83e5a0babf23ce960bbc2defd284f03
Flipworld <talga.tawit@gmail.com>
2026-07-07 06:35:17 +0500

    seed 3 fake comments on Video creation

--- videos/signals.py
+++ videos/signals.py
@@ -1,15 +1,31 @@
-#
-# from django.db.models.signals import  post_save
-# from django.dispatch import receiver
-# from videos.tasks import video_encode
-# from videos.models import Video
-#
-#
-# @receiver(post_save, sender=Video)
-# def video_signal(sender, instance, created,*args, **kwargs):
-#      if instance and created:
-#         video_encode.delay(3,instance.id)
-#         print("Task initiated")
-#      else:
-#         # add your logic here
-#         pass
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from videos.models import Video, VideoComment
+from comments.models import Comment
+
+
+FAKE_COMMENTS = [
+    "Nice video! 🔥",
+    "Great content, keep it up!",
+    "Thanks for sharing this!",
+]
+
+
+@receiver(post_save, sender=Video)
+def seed_fake_comments(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    author = instance.user
+    if not author:
+        return
+
+    for text in FAKE_COMMENTS:
+        comment = Comment.objects.create(
+            user=author,
+            text=text,
+        )
+        VideoComment.objects.create(
+            comment=comment,
+            video=instance,
+        )
